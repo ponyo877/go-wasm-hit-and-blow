ebbf267a25c9af30c2f5eac8123972bae11ed8d8
日高敬介 <keisuke877jp@yahoo.co.jp>
2024-08-05 23:26:16 +0900

    :sparkles: 基本ルールの実装完了

--- main.go
+++ main.go
@@ -15,7 +15,8 @@ import (
 	"time"
 
 	"github.com/pion/webrtc/v3"
-	"github.com/ponyo877/go-wasm-p2p-chat/go-ayame"
+	"github.com/ponyo877/go-wasm-hit-and-blow/game"
+	"github.com/ponyo877/go-wasm-hit-and-blow/go-ayame"
 
 	"nhooyr.io/websocket"
 	"nhooyr.io/websocket/wsjson"
@@ -25,7 +26,7 @@ var (
 	wsScheme          string
 	matchmakingOrigin string
 	signalingOrigin   string
-	recentGuess       *Guess
+	recentGuess       *game.Guess
 )
 
 type mmReqMsg struct {
@@ -55,7 +56,7 @@ func main() {
 	}
 	var resMsg mmResMsg
 	var dc *webrtc.DataChannel
-	ch := make(chan *Guess)
+	ch := make(chan *game.Guess)
 	defer func() {
 		if dc != nil {
 			dc.Close()
@@ -63,8 +64,8 @@ func main() {
 	}()
 	var conn *ayame.Connection
 	connected := make(chan bool)
-	board := NewBoard()
-	js.Global().Set("startSearchPlayer", js.FuncOf(func(_ js.Value, _ []js.Value) interface{} {
+	board := game.NewBoard()
+	js.Global().Set("Search", js.FuncOf(func(_ js.Value, _ []js.Value) interface{} {
 		go func() {
 			ws, _, err := websocket.Dial(context.Background(), mmURL.String(), nil)
 			if err != nil {
@@ -89,26 +90,32 @@ func main() {
 			if resMsg.Type == "MATCH" {
 				conn = ayame.NewConnection(signalingURL.String(), resMsg.RoomID, ayame.DefaultOptions(), false, false)
 				conn.OnOpen(func(metadata *interface{}) {
-					log.Println("Open")
 					var err error
-					dc, err = conn.CreateDataChannel("matchmaking-hit&blow", nil)
+					dc, err = conn.CreateDataChannel("matchmaking-example", nil)
 					if err != nil && err != fmt.Errorf("client does not exist") {
 						log.Printf("CreateDataChannel error: %v", err)
 						return
 					}
 					log.Printf("CreateDataChannel: label=%s", dc.Label())
-					rand.NewSource(time.Now().UnixNano())
-					seed := rand.Int()
+					go func() {
+						rand.NewSource(time.Now().UnixNano())
+						seed := rand.Int()
 
-					initTurn := NewTurnBySeed(seed)
-					myHand := NewHandBySeed(seed)
-					board.Start(myHand, initTurn)
-					startMsg := Message{Type: "start", Turn: int(initTurn)}
-					by, _ := json.Marshal(startMsg)
-					if err := dc.SendText(string(by)); err != nil {
-						handleError()
-						return
-					}
+						initTurn := game.NewTurnBySeed(seed)
+						myHand := game.NewHandBySeed(seed)
+						log.Printf("myHand(opener): %v", myHand)
+						board.Start(myHand, initTurn)
+						turn := int(initTurn)
+						startMsg := Message{Type: "start", Turn: &turn}
+						by, _ := json.Marshal(startMsg)
+						time.Sleep(3 * time.Second)
+						log.Printf("startMsg(opener): %v", string(by))
+						if err := dc.SendText(string(by)); err != nil {
+							log.Printf("failed to send startMsg: %v", err)
+							return
+						}
+					}()
+
 					dc.OnMessage(onMessage(dc, ch, board))
 				})
 
@@ -123,11 +130,12 @@ func main() {
 					if dc == nil {
 						dc = c
 					}
+					log.Println("ready to recieve")
 					dc.OnMessage(onMessage(dc, ch, board))
 				})
 
 				if err := conn.Connect(); err != nil {
-					log.Fatal("Failed to connect Ayame", err)
+					log.Fatal("failed to connect Ayame", err)
 				}
 				select {
 				case <-connected:
@@ -137,7 +145,7 @@ func main() {
 		}()
 		return js.Undefined()
 	}))
-	js.Global().Set("sendMessage", js.FuncOf(func(_ js.Value, _ []js.Value) interface{} {
+	js.Global().Set("Guess", js.FuncOf(func(_ js.Value, _ []js.Value) interface{} {
 		go func() {
 			el := getElementByID("message")
 			message := el.Get("value").String()
@@ -148,8 +156,12 @@ func main() {
 			if dc == nil {
 				return
 			}
+			// if err := dc.SendText("ping"); err != nil {
+			// 	handleError()
+			// 	return
+			// }
 
-			ch <- NewGuessFromText(message)
+			ch <- game.NewGuessFromText(message)
 			logElem(fmt.Sprintf("[You]: %s\n", message))
 			el.Set("value", "")
 		}()
@@ -168,31 +180,34 @@ func shortHash(now time.Time) (string, error) {
 
 type Message struct {
 	Type  string `json:"type"`
-	Turn  int    `json:"turn,omitempty"`
-	Hit   int    `json:"hit,omitempty"`
-	Blow  int    `json:"blow,omitempty"`
+	Turn  *int   `json:"turn,omitempty"`
+	Hit   *int   `json:"hit,omitempty"`
+	Blow  *int   `json:"blow,omitempty"`
 	Guess string `json:"guess,omitempty"`
 }
 
-func onMessage(dc *webrtc.DataChannel, ch chan *Guess, board *Board) func(webrtc.DataChannelMessage) {
+func onMessage(dc *webrtc.DataChannel, ch chan *game.Guess, board *game.Board) func(webrtc.DataChannelMessage) {
 	return func(msg webrtc.DataChannelMessage) {
+		log.Printf("recieve msg.Data: %s", string(msg.Data))
 		if !msg.IsString {
 			return
 		}
 		var message Message
 		if err := json.Unmarshal(msg.Data, &message); err != nil {
-			log.Printf("Failed to unmarshal: %v", err)
+			log.Printf("failed to unmarshal: %v", err)
 			return
 		}
-		logElem(fmt.Sprintf("[Any]: %s\n", msg.Data))
+		// logElem(fmt.Sprintf("[Any]: %s\n", msg.Data))
 		switch message.Type {
 		case "start":
 			// 非開室者Only: GameStart処理
 			if board.IsInMenu() {
-				initTurn := Turn(message.Turn).Reverse()
+				log.Printf("message.Turn: %v", *message.Turn)
+				initTurn := game.Turn(*message.Turn).Reverse()
 				rand.NewSource(time.Now().UnixNano())
 				seed := rand.Int()
-				myHand := NewHandBySeed(seed)
+				myHand := game.NewHandBySeed(seed)
+				log.Printf("myHand(unopener): %v", myHand)
 				board.Start(myHand, initTurn)
 			}
 			// 非開室者Only: 初回が後攻のときに開室者を初回guess処理に誘導
@@ -200,9 +215,10 @@ func onMessage(dc *webrtc.DataChannel, ch chan *Guess, board *Board) func(webrtc
 				startMsg := Message{Type: "start"}
 				by, _ := json.Marshal(startMsg)
 				if err := dc.SendText(string(by)); err != nil {
-					handleError()
+					log.Printf("failed to send startMsg: %v", err)
 					return
 				}
+				log.Printf("startMsg(unopener): %v", string(by))
 				return
 			}
 			// guess送信処理に続く
@@ -212,12 +228,14 @@ func onMessage(dc *webrtc.DataChannel, ch chan *Guess, board *Board) func(webrtc
 			}
 			// 自分ターンへ遷移
 			board.ToggleTurn()
-			guess := NewGuessFromText(message.Guess)
+			guess := game.NewGuessFromText(message.Guess)
 			ans := board.CalcAnswer(guess)
-			ansMsg := Message{Type: "answer", Hit: ans.Hit(), Blow: ans.Blow()}
+			hit, blow := ans.Hit(), ans.Blow()
+			ansMsg := Message{Type: "answer", Hit: &hit, Blow: &blow}
 			by, _ := json.Marshal(ansMsg)
+			log.Printf("ansMsg: %v", string(by))
 			if err := dc.SendText(string(by)); err != nil {
-				handleError()
+				log.Printf("failed to send ansMsg: %v", err)
 				return
 			}
 			if ans.IsAllHit() {
@@ -230,8 +248,8 @@ func onMessage(dc *webrtc.DataChannel, ch chan *Guess, board *Board) func(webrtc
 			if board.IsMyTurn() {
 				return
 			}
-			ans := NewAnswer(message.Hit, message.Blow)
-			qa := NewQA(recentGuess, ans)
+			ans := game.NewAnswer(*message.Hit, *message.Blow)
+			qa := game.NewQA(recentGuess, ans)
 			board.AddMyQA(qa)
 			if ans.IsAllHit() {
 				logElem("[Sys]: You Win!\n")
@@ -257,7 +275,7 @@ func onMessage(dc *webrtc.DataChannel, ch chan *Guess, board *Board) func(webrtc
 			toMsg := Message{Type: "timeout"}
 			by, _ := json.Marshal(toMsg)
 			if err := dc.SendText(string(by)); err != nil {
-				handleError()
+				log.Printf("failed to send toMsg: %v", err)
 				return
 			}
 			logElem("[Sys]: You Timeout! You Lose!\n")
@@ -269,7 +287,7 @@ func onMessage(dc *webrtc.DataChannel, ch chan *Guess, board *Board) func(webrtc
 		// 相手ターンへ遷移
 		board.ToggleTurn()
 		if err := dc.SendText(string(by)); err != nil {
-			handleError()
+			log.Printf("failed to send guessMsg: %v", err)
 			return
 		}
 	}
